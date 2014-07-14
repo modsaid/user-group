b22f654ecac46eb04ee781b8a967335f625285a4
shehio <shehio_22@hotmail.com>
2014-07-14 10:54:55 +0200

    fixed some bugs in group views, and a bug in show-bootstrap, and customized the show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_group, only: [ :show, :edit, :update, :destroy ]
   def index
     @groups = Group.all
   end
