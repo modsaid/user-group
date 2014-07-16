d8d9391cd9e1c77f2df35ca0785430211eb1d446
shehio <shehio_22@hotmail.com>
2014-07-16 13:24:54 +0200

    updated the testing of group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,1 +1,16 @@
-
+require 'rails_helper'
+require 'faker'
+describe Group do
+  it "must have a title" do
+    group1 = Group.new
+    group1.title = "shehios_group"
+    group1.title.should_not eql?(nil) 
+  end
+  it  "has a unique title" do
+    group1 = Group.new
+    group1.title = "shehios_group"
+    group2 = Group.new
+    group2.title = "not_shehios_group"
+    expect(group1.title).should_not eql?(group2.title)
+  end
+end
