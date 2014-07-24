29627f6f78d063dcc403779da88bb213ff95cf34
modsaid <mahmoud@modsaid.com>
2014-07-24 12:27:04 +0200

    customizing simplecov report to consider rails project sections

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,14 @@
 #
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start  'rails' do
+  add_filter '/test/'
+  add_filter '/config/'
+
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+end
 
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
